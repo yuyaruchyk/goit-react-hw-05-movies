99f04cea5084eb4e695eeceacb7a27f797360633
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-25 22:41:02 +0100

    upd
    upd

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,19 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Searchbar } from 'components/SearchForm/SearchForm';
 import MovieItem from 'components/MovieItem/MovieItem';
 import { getSearch } from 'components/Api';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
-  const [searchText, setSearchText] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryNew = searchParams.get('query') ?? '';
   const [searchedMoviesList, setSearchedMoviesList] = useState([]);
 
   useEffect(() => {
-    if (searchText === '') {
+    if (queryNew === '') {
       return;
     }
 
     async function getSearchedMovies() {
       try {
-        const newQuery = searchText.split('/').pop();
+        const newQuery = queryNew.split('/').pop();
         const response = await getSearch(newQuery);
         const foundMovies = response.results;
         setSearchedMoviesList(foundMovies);
@@ -24,17 +26,17 @@ const Movies = () => {
     }
 
     getSearchedMovies();
-  }, [searchText]);
+  }, [queryNew]); 
 
-  const handleSubmit = value => {
-    setSearchText(`${Date.now()}/${value}`);
+  const handleSubmit = (value) => {
+    setSearchParams({ query: value });
   };
 
   return (
     <div>
       <Searchbar onSubmit={handleSubmit} />
       <ul>
-        {searchedMoviesList.map(movie => (
+        {searchedMoviesList.map((movie) => (
           <MovieItem key={movie.id} id={movie.id} title={movie.title} />
         ))}
       </ul>
