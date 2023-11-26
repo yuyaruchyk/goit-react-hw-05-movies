ae6676b98d7bc352b5f249416c36e1cdc0eb0061
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-26 17:40:30 +0100

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { lazy } from "react";
 
 const Layout = lazy(() => import("../pages/Layout/Layout"));
 const Home = lazy(() => import("pages/Home/Home"));
-const NotFoundPage = lazy(() => import("pages/NotFoundPage"));
-const Movies = lazy(() => import("pages/Movies"));
+const NotFoundPage = lazy(() => import("pages/NotFoundPage/NotFoundPage"));
+const Movies = lazy(() => import("pages/Movies/Movies"));
 const Cast = lazy(() => import("components/Cast/Cast"));
 const Reviews = lazy(() => import("components/Reviews/Reviews"));
 const MovieDetails = lazy(() => import("pages/MovieDetails/MovieDetails"));
