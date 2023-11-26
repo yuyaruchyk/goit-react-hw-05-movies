275d889926bad92a8b7262585462139680367134
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-26 15:56:51 +0000

    Deploying to gh-pages from @ yuyaruchyk/goit-react-hw-05-movies@8977060790342a13159a2a83e47be646433b5725 🚀

---
+++ static/js/520.eee31384.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,r,t){t.r(r),t.d(r,{default:function(){return a}});var n=t(87),o=t(184);function a(){return(0,o.jsxs)("div",{children:["Sorry! Error! Please use this ",(0,o.jsx)(n.rU,{to:"/",children:"link"})," to navigate to our home page"]})}}}]);
+//# sourceMappingURL=520.eee31384.chunk.js.map
